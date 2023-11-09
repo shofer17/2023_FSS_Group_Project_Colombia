754cf84225747879b7350bdfe5f12c532deffa06
Silvan Hofer <sm.hofer@blueiwn.ch>
2023-11-09 16:49:05 +0100

    clean code

--- 0 data prep.R
+++ 0 data prep.R
@@ -2,19 +2,10 @@
 # Author: Silvan Hofer
 # Purpose: Data prep for Group project
 
-setwd("../")
-
-path_raw = "data_raw/"
-path_clean = "data_clean/"
-path_charts = "charts/"
+setwd("../") #set the working environment to "2023_FSS_Group_Project"
+source("2023_FSS_Group_Project_Colombia/terms_and_definitions.R") #load in common terms and defintions    
 
 limit  <- 100000 # dataset has 60k rows, 100k should get all data
-
-
-# Libraries
-library(tidyverse)
-
-
 
 # 1. Load data ----------------------------------------------------------------- 
 ## 1.1 Death data --------------------------------------------------------------
@@ -24,8 +15,6 @@ data_deaths_complete_2 <- read.csv(paste0(path_raw, "Mort2018US.PubUse.txt"))
 
 # https://data.cdc.gov/NCHS/VSRR-Provisional-Drug-Overdose-Death-Counts/xkb8-kh2a
 data_VSRR <- read.csv("https://data.cdc.gov/resource/xkb8-kh2a.csv?$limit=100000")
-data_VSRR_2 <- read.csv("https://data.cdc.gov/resource/xkb8-kh2a.csv?$period=1")
-
 
 # https://injuryfacts.nsc.org/home-and-community/safety-topics/drugoverdoses/data-details/
 data_injuryfacts <- readxl::read_xlsx(paste0(path_raw, 
@@ -33,7 +22,6 @@ data_injuryfacts <- readxl::read_xlsx(paste0(path_raw,
                                  skip = 1)
 
 ## 1.2 Drug data --------------------------------------------------------------
-
 
 drugs_nw_20_23 <- read.csv(paste0(path_raw, "nationwide-drugs-fy20-fy23.csv"))
 drugs_nw_19_22 <- read.csv(paste0(path_raw, "nationwide-drugs-fy19-fy22.csv"))
@@ -43,16 +31,12 @@ drugs_amo_19_22 <- read.csv(paste0(path_raw, "amo-drug-seizures-fy19-fy22.csv"))
 
 
 # 2. prep data -----------------------------------------------------------------
-
-
-
 # 2.1 deaths -----------------------------------------------------------------
 
 data_VSRR_prov <- data_VSRR %>%
   mutate(year_month = paste0(year, "_", month))%>%
   select(year_month, indicator, data_value)%>%
   filter(indicator %in% c("Synthetic opioids, excl. methadone (T40.4)", "Cocaine (T40.5)"))
-
 
 data_injuryfacts <- data_injuryfacts %>% 
   filter(Gender == "Both sexes" &
@@ -63,42 +47,52 @@ data_injuryfacts <- data_injuryfacts %>%
          drug %in% c("Cocaine", "Opioid subgroup – including fentanyl"))%>%
   mutate(number = as.numeric(number))
 
-
 # 2.2 drugs seized -----------------------------------------------------------------
-
 
 drugs_amo_19_22 <- drugs_amo_19_22 %>% select(-Branch) %>% filter(FY == 2019)
 drugs_amo_20_23 <- drugs_amo_20_23 %>% select(-Branch)
 drugs_nw_19_22 <- drugs_nw_19_22 %>% select(-Area.of.Responsibility)%>% filter(FY == 2019)
 drugs_nw_20_23 <- drugs_nw_20_23 %>% select(-Area.of.Responsibility)
-
 drugs <- unique(rbind(drugs_amo_19_22, drugs_amo_20_23, drugs_nw_19_22, drugs_nw_20_23))
+
+
+
+months <- data.frame("month" = c("JAN","FEB","MAR","APR","MAY","JUN", "JUL", "AUG","SEP","OCT","NOV","DEC"),
+                     "month_num" = c(paste0("0", 1:9),10:12))
 
 drugs <- drugs %>%
     rename("year" = "FY", "month" = "Month..abbv.", "drug" = "Drug.Type", "quantity" = "Sum.Qty..lbs.")%>%
     select(year,month, drug, quantity)%>%
     filter(drug %in% c("Cocaine", "Fentanyl"))%>%
     group_by(year, month, drug)%>%
-    summarise(total = sum(quantity))%>%
+    summarise(kg = sum(quantity))%>%
     ungroup()%>%
-    mutate(ym = paste0(year, "_", month))
+    left_join(months, by = "month")%>%
+    mutate(date = dmy(paste0("1-",month_num,"-", year)))%>% #for plotting
+    mutate(date_stata = paste0(year,month_num))%>% #for merge with stata
+    select(-c(year, month_num,month))
 
+
+
+rm(drugs_amo, drugs_amo_19_22, drugs_amo_20_23, drugs_nw, drugs_nw_20_23, drugs_nw_19_22, months)
 
 # 3 save data -----------------------------------------------------------------
 
 writexl::write_xlsx(data_deaths, path = paste0(path_clean, "death_by_drug.xlsx"))
 writexl::write_xlsx(drugs, path = paste0(path_clean, "seized_by_drug.xlsx"))
 
-
 # 4 plot data -----------------------------------------------------------------
 
 p <- ggplot(data_deaths, aes(x = year, y = number, color = drug))+
-  geom_line()
+  geom_line();p
 
-p
 
-p1 <- ggplot(drugs, aes(x = year, y = number, color = drug))+
-  geom_line()
+p1 <- ggplot(drugs, aes(x = date, y = log(total), color = drug))+
+  geom_line(); p1
 
-p1
+# 5 ave plots -----------------------------------------------------------------
+
 ggsave(paste0(path_charts, "death_by_drug.png"),p)
+ggsave(paste0(path_charts, "seized_by_drug.png"),p1)
+
+
---
+++ terms_and_definitions.R
@@ -0,0 +1,11 @@
+
+#sys definitions
+Sys.setenv(LANGUAGE="en")
+
+#paths
+path_raw = "data_raw/"
+path_clean = "data_clean/"
+path_charts = "charts/"
+
+# Libraries
+library(tidyverse)
