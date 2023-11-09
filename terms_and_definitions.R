a6196f97ad17e21e25b5ac53d1c2cd8af45d1c2a
Silvan Hofer <sm.hofer@blueiwn.ch>
2023-11-09 17:13:43 +0100

    change data format

--- terms_and_definitions.R
+++ terms_and_definitions.R
@@ -1,10 +1,14 @@
+# This document provides all shared terms and definitions for the project. 
+# Additionally, libraries and system changes are included to make life easier. 
+
+
 
 #sys definitions
 Sys.setenv(LANGUAGE="en")
 
 #paths
 path_raw = "data_raw/"
-path_clean = "data_clean/"
+path_clean = "2023_FSS_Group_Project_Colombia/data_clean/"
 path_charts = "charts/"
 
 # Libraries
