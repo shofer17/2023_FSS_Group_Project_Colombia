e208840e9ba5d749e5b2f5f07b17fc828cd5c200
Silvan Hofer <sm.hofer@blueiwn.ch>
2023-11-24 16:19:33 +0100

    clean up code and adjust path raw

--- 0 prep mortality.R
+++ 0 prep mortality.R
@@ -1,20 +1,30 @@
+# Date: 06/11/2023
+# Author: Silvan Hofer
+# Purpose: Data prep for Group project
+# Please note: Due to the data size, the raw data was loaded from the desktop
+# and not from the cloud. The data was downloaded from: 
+# https://www.nber.org/research/data/mortality-data-vital-statistics-nchs-multiple-cause-death-data
+# and then processed using the code below. 
+
 setwd("I:/Meine Ablage/2023_FSS_Group_Project")
 source("2023_FSS_Group_Project_Colombia/terms_and_definitions.R") #load in common terms and defintions    
-
-
 setwd("~/FSS_Group_Project_Nightlight")
 
 years <- 2012:2021
 out <- data.frame()
-for(i in 4:length(years)){
+
+# The loop loads in all full mortality file one by one, then processes it and then
+# adds the output to the "out" frame. The "out" frame represents the final data
+# which is also uploaded to GitHub. 
+for(i in 1:length(years)){
   
   path <- paste0("mortality_files/mort", years[i], ".csv")
   
   data_loop <- read_csv(path)
   data_loop$id <- 1:nrow(data_loop)
-  data_loop<- data_loop[,grepl("enicon|monthdth|year|id", names(data_loop))]
+  data_loop<- data_loop[,grepl("enicon|monthdth|year|id", names(data_loop))] #grab relevant variables
   
-  data_loop$is_cocaine <- apply(data_loop, 1, FUN = function(x) any(na.omit(x) == "T405"))
+  data_loop$is_cocaine <- apply(data_loop, 1, FUN = function(x) any(na.omit(x) == "T405")) #grab deaths related to cocaine and fentanyl
   data_loop$is_fentanyl <- apply(data_loop, 1, FUN = function(x) any(na.omit(x) == "T404"))
   
   
